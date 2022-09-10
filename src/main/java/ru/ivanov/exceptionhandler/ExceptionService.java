41c6527eb29a3c9e3b2189979bf245b5717ae781
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-09-10 11:47:09 +0300

    extends RuntimeException

--- src/main/java/ru/ivanov/exceptionhandler/ExceptionService.java
+++ src/main/java/ru/ivanov/exceptionhandler/ExceptionService.java
@@ -1,12 +1,11 @@
 package ru.ivanov.exceptionhandler;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExceptionService {
 
   public void methodThrowsException(){
-    throw new PaymentRequiredException(HttpStatus.PAYMENT_REQUIRED, "Требуется оплата");
+    throw new PaymentRequiredException("Требуется оплата");
   }
 }
--- src/main/java/ru/ivanov/exceptionhandler/PaymentRequiredException.java
+++ src/main/java/ru/ivanov/exceptionhandler/PaymentRequiredException.java
@@ -1,11 +1,8 @@
 package ru.ivanov.exceptionhandler;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+public class PaymentRequiredException extends RuntimeException {
 
-public class PaymentRequiredException extends ResponseStatusException {
-
-  public PaymentRequiredException(HttpStatus status, String reason) {
-    super(status, reason);
+  public PaymentRequiredException( String reason) {
+    super(reason);
   }
 }
