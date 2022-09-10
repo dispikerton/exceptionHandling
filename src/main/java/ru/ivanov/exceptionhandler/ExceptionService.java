678765a867da66cf57e11b7c3d17ea290aaa04dc
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-09-10 11:55:15 +0300

    throw ResponseStatusException

--- src/main/java/ru/ivanov/exceptionhandler/ExceptionService.java
+++ src/main/java/ru/ivanov/exceptionhandler/ExceptionService.java
@@ -1,11 +1,21 @@
 package ru.ivanov.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ExceptionService {
 
-  public void methodThrowsException(){
+  public void methodThrowsException() {
+    try {
+      methodWherePaymentIsRequired();
+    } catch (PaymentRequiredException e) {
+      throw new ResponseStatusException(HttpStatus.PAYMENT_REQUIRED, e.getMessage());
+    }
+  }
+
+  private void methodWherePaymentIsRequired() {
     throw new PaymentRequiredException("Требуется оплата");
   }
 }
