fca62fe80730d755600f6cbb1d6e3e1fcbd41ce9
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-09-13 11:04:15 +0300

    @ExceptionHandler

--- src/main/java/ru/ivanov/exceptionhandler/Controller.java
+++ src/main/java/ru/ivanov/exceptionhandler/Controller.java
@@ -1,5 +1,8 @@
 package ru.ivanov.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,10 @@ public class Controller {
   @GetMapping
   public void method() {
     service.methodThrowsException();
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<String> handle(PaymentRequiredException exception) {
+    return new ResponseEntity<>(exception.getMessage(), HttpStatus.PAYMENT_REQUIRED);
   }
 }
